c3236540bbb5fce3bae86a7f461761f94159e798
FauzanEdris <fauzanedris@yahoo.com>
2018-06-29 15:42:23 +0800

    UAS

---
+++ application/views/_layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+		Dashboard Admin
+	</div>
+	<!-- Default to the left -->
+	<strong>Pemrograman Web <a href="https://stikom-bali.ac.id/">STIKOM BALI</a>.</strong>
+</footer>
---
+++ database/dbuasweb.sql
@@ -0,0 +1,200 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.0.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2018 at 09:20 AM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbuasweb`
+--
+CREATE DATABASE IF NOT EXISTS `dbuasweb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dbuasweb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `foto`) VALUES
+(1, 'Fauzan Edris', '21232f297a57a5a743894a0e4a801fc3', 'M. Handri Fauzan R. Edris', 'profil1.jpg'),
+(2, 'P Web', '099bb5a2274c835f97d9e44bf82c7b1e', 'Pemrograman Web', 'profil2.png'),
+(3, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'profil3.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelamin`
+--
+
+DROP TABLE IF EXISTS `kelamin`;
+CREATE TABLE `kelamin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelamin`
+--
+
+INSERT INTO `kelamin` (`id`, `nama`) VALUES
+(1, 'Laki laki'),
+(2, 'Perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota`
+--
+
+DROP TABLE IF EXISTS `kota`;
+CREATE TABLE `kota` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kota`
+--
+
+INSERT INTO `kota` (`id`, `nama`) VALUES
+(1, 'Denpasar'),
+(3, 'Badung'),
+(4, 'Tulungagung'),
+(17, 'Jakarta'),
+(21, 'Surabaya'),
+(22, 'Bandung');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+DROP TABLE IF EXISTS `pegawai`;
+CREATE TABLE `pegawai` (
+  `id` varchar(255) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `telp` varchar(255) DEFAULT NULL,
+  `id_kota` int(11) DEFAULT NULL,
+  `id_kelamin` int(1) DEFAULT NULL,
+  `id_posisi` int(11) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`id`, `nama`, `telp`, `id_kota`, `id_kelamin`, `id_posisi`, `status`) VALUES
+('10', 'Antony Febriansyah Hartono', '082199568540', 1, 1, 1, 1),
+('11', 'Hafizh Asrofil Al Banna', '087859615271', 1, 1, 1, 1),
+('12', 'Faiq Fajrullah', '085736333728', 1, 1, 2, 1),
+('3', 'Mustofa Halim', '081330493322', 1, 1, 3, 1),
+('4', 'Dody Ahmad Kusuma Jaya', '083854520015', 1, 1, 2, 1),
+('5', 'Mokhammad Choirul Ikhsan', '085749535400', 3, 1, 2, 1),
+('7', 'Achmad Chadil Auwfar', '08984119934', 2, 1, 1, 1),
+('8', 'Rizal Ferdian', '087777284179', 1, 1, 3, 1),
+('9', 'Redika Angga Pratama', '083834657395', 1, 1, 3, 1),
+('1', 'Tolkha Hasan', '081233072122', 1, 1, 4, 1),
+('2', 'Wawan Dwi Prasetyo', '085745966707', 4, 1, 4, 1),
+('10b9bd2c7b1e414e1b50153c07c47de6', 'Muhammad Noor Hadi', '081999390078', 3, 1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posisi`
+--
+
+DROP TABLE IF EXISTS `posisi`;
+CREATE TABLE `posisi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posisi`
+--
+
+INSERT INTO `posisi` (`id`, `nama`) VALUES
+(1, 'IT'),
+(2, 'HRD'),
+(3, 'Keuangan'),
+(4, 'Produk'),
+(5, 'Web Developer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelamin`
+--
+ALTER TABLE `kelamin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posisi`
+--
+ALTER TABLE `posisi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `kota`
+--
+ALTER TABLE `kota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `posisi`
+--
+ALTER TABLE `posisi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
